5079572e800eec4e4d62a980b0e05d0c5cb93007
PrabuKannan-Dev <prabudotkay@gmail.com>
2024-08-21 14:48:21 +0530

    get all products implemented using fakestore

--- src/main/java/com/example/ecomm/services/ProductService.java
+++ src/main/java/com/example/ecomm/services/ProductService.java
@@ -2,6 +2,9 @@ package com.example.ecomm.services;
 
 import com.example.ecomm.models.Product;
 
+import java.util.List;
+
 public interface ProductService {
     Product createProduct(Product product);
+    List<Product> getProducts();
 }
